7c37a380dae57ce729d9e4687aedb155b00ca6f8
ABCDWJNIDAWHUIJN <142064828+ABCDWJNIDAWHUIJN@users.noreply.github.com>
2026-03-21 11:03:09 +0000

    Update ScreenshareStart.ps1

--- ScreenshareStart.ps1
+++ ScreenshareStart.ps1
@@ -1,12 +1,13 @@
-# ScreenShareStart.ps1 - Opens folders and launches all tools
-
 # Open folders
 explorer shell:recent
 explorer $env:TEMP
 explorer shell:windows
 
-# Launch all tools in separate windows
+# Launch all tools
 Start-Process powershell -ArgumentList '-NoExit', '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', 'Invoke-Expression (Invoke-RestMethod ''https://raw.githubusercontent.com/Unknown-dll/Tool-Collector/refs/heads/main/Tools%20Collector'')'
+
 Start-Process powershell -ArgumentList '-NoExit', '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', 'Invoke-Expression (Invoke-RestMethod ''https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/Services.ps1'')'
+
 Start-Process powershell -ArgumentList '-NoExit', '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', 'Invoke-Expression (Invoke-RestMethod ''https://raw.githubusercontent.com/MeowTonynoh/MeowModAnalyzer/main/MeowModAnalyzer.ps1'')'
+
 Start-Process powershell -ArgumentList '-NoExit', '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', 'Invoke-Expression (Invoke-RestMethod ''https://raw.githubusercontent.com/ABCDWJNIDAWHUIJN/SS-TOOL/main/Alt-Detector.ps1'')'

9039ab2c261198a813412d46da05f2a1362927c7
ABCDWJNIDAWHUIJN <142064828+ABCDWJNIDAWHUIJN@users.noreply.github.com>
2026-03-21 10:59:29 +0000

    Update ScreenshareStart.ps1

--- ScreenshareStart.ps1
+++ ScreenshareStart.ps1
@@ -1,12 +1,1 @@
-# ScreenShareStart.ps1 - Opens folders and launches all tools
-
-# Open folders
-explorer shell:recent
-explorer $env:TEMP
-explorer shell:windows
-
-# Launch all tools in separate windows
-Start-Process powershell -ArgumentList '-NoExit', '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', 'Invoke-Expression (Invoke-RestMethod ''https://raw.githubusercontent.com/Unknown-dll/Tool-Collector/refs/heads/main/Tools%20Collector'')'
-Start-Process powershell -ArgumentList '-NoExit', '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', 'Invoke-Expression (Invoke-RestMethod ''https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/Services.ps1'')'
-Start-Process powershell -ArgumentList '-NoExit', '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', 'Invoke-Expression (Invoke-RestMethod ''https://raw.githubusercontent.com/MeowTonynoh/MeowModAnalyzer/main/MeowModAnalyzer.ps1'')'
-Start-Process powershell -ArgumentList '-NoExit', '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', 'Invoke-Expression (Invoke-RestMethod ''https://raw.githubusercontent.com/ABCDWJNIDAWHUIJN/SS-TOOL/main/AltDetector.ps1'')'
+powershell -NoProfile -ExecutionPolicy Bypass -Command "explorer shell:recent; explorer $env:TEMP; explorer shell:windows; start powershell -ArgumentList '-NoExit', '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', 'Invoke-Expression (Invoke-RestMethod ''https://raw.githubusercontent.com/Unknown-dll/Tool-Collector/refs/heads/main/Tools%20Collector'')'; start powershell -ArgumentList '-NoExit', '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', 'Invoke-Expression (Invoke-RestMethod ''https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/Services.ps1'')'; start powershell -ArgumentList '-NoExit', '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', 'Invoke-Expression (Invoke-RestMethod ''https://raw.githubusercontent.com/MeowTonynoh/MeowModAnalyzer/main/MeowModAnalyzer.ps1'')'; start powershell -ArgumentList '-NoExit', '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', 'Invoke-Expression (Invoke-RestMethod ''https://raw.githubusercontent.com/ABCDWJNIDAWHUIJN/SS-TOOL/main/Alt-Detector.ps1'')'"
